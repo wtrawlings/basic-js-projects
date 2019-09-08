dc95b9a04e203681c4e5c5ffd59858ce19d5698d
Warren Rawlings <rawlings.warren@gmail.com>
2019-09-08 17:32:11 -0400

    red and green colors for number

--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,31 @@
 let counter = document.querySelector('.counter');
-const addCont = document.querySelector('#addCountBtn');
-const lowerCont = document.querySelector('#lowerCountBtn');
+const addCount = document.querySelector('#addCounterBtn');
+const lowerCount = document.querySelector('#lowerCounterBtn');
 
 let count = 0;
-addCont.addEventListener('click', incrementCounter);
-lowerCont.addEventListener('click', decrementCounter);
+
+
+addCount.addEventListener('click', incrementCounter);
+lowerCount.addEventListener('click', decrementCounter);
+
+function incrementCounter() {
+    count++;
+    counter.innerHTML = count;
+    if (counter.innerHTML > 0) {
+        counter.style.color = '#4caf50';
+    } else if (counter.innerHTML === 0) {
+        counter.style.color = 'white';
+    } else { counter.style.color = 'red' }
+}
+
+
+
+function decrementCounter() {
+    count--;
+    counter.innerHTML = count;
+    if (counter.innerHTML > 0) {
+        counter.style.color = '#4caf50';
+    } else if (counter.innerHTML === 0) {
+        counter.style.color = 'white';
+    } else { counter.style.color = 'red' }
+}
